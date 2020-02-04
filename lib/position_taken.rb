52e135979552f0d8edb6b0b74f2cdfcab9f3e981
Trevor Jones <tjones009@yahoo.com>
2020-02-04 16:26:35 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,28 @@
-# code your #position_taken? method here!
+# # code your #position_taken? method here!
+# def position_taken?(board, index)
+#   ((board[index] == ("X") || (board[index] == "O"))
+ 
+# if board[index] == "X"
+#   TRUE
+# elsif board[index] == "O"
+#   TRUE
+# else
+#   FALSE
+# end
+ 
+#   #if board[index] == " "
+#     #FALSE
+#   #end
+  
+#   #!(board[index] == " ")
+# #end
+
+def is_move_taken?(board,position)
+  position.to_i.between?(1,9) && position_taken?(board, position.to_i - 1)
+end
+
+#valid position
+def position_taken?(board,position)
+  return true if ["X", "O"].include?(board[position])
+  return false if [" ", "", nil].include?(board[position])
+end
